eee04d543668604d255510b3a65394219e9261ea
Bob2518 <larnesta@icloud.com>
2017-11-28 15:51:48 +0000

    d

--- Beginning of AW/node1example/nodeexample.js
+++ Beginning of AW/node1example/nodeexample.js
@@ -1,14 +1,4 @@
 var express = require("express");
-
 var server = express();
-
-server.get("/", (req, res) => {
-    res.send("Hello");
-});
-
-
-server.get("/dogs", (req, res) => {
-    res.send("My dog's name is jeff");
-});
-
+server.use(express.static("src"))
 server.listen(3000);
